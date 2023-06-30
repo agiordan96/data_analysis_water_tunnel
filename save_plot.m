97dc853ea753ed9ea259e91e693c69628605f895
Andrea <a.giordano21@imperial.ac.uk>
2023-06-30 16:06:58 +0200

    partial adaptation to Fourier plots

--- save_plot.m
+++ save_plot.m
@@ -3,6 +3,9 @@ function [] = save_plot(gcf, plot_type, plot_variable, j, speed, format)
 
 if length(plot_type) == 5 && plot_type == "title"
     saveas(gcf, ['./pictures_wing/pic_hardwing/', date, '/', plot_variable, '_plot/', plot_variable, '_plot_#', num2str(j), '_flow_speed_0_', num2str(100 * speed)], format);
+
+elseif length(plot_type) == 7 && plot_type == "fourier"
+    saveas(gcf, ['./pictures_wing/pic_hardwing/', date, '_', plot_type, '/', plot_variable, '_plot/', plot_variable, '_plot_', num2str(j), '_deg_flow_speed_0_', num2str(speed)], format);
 else
     saveas(gcf, ['./pictures_wing/pic_hardwing/', date, '_', plot_type, '/', plot_variable, '_plot/', plot_variable, '_plot_#', num2str(j), '_flow_speed_0_', num2str(100 * speed)], format);
 
