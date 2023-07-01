c95d02792ee472b550da19d3528b9d06edf58357
Andrea <a.giordano21@imperial.ac.uk>
2023-07-01 14:19:18 +0200

    adaptations to soft wing plots

--- plot_CLCD.m
+++ plot_CLCD.m
@@ -1,65 +1,158 @@
-function [] = plot_CLCD(exp_value, sel_speed, div, chord, kin_viscosity, plot_type, format)
+function [] = plot_CLCD(wingtype, sel_inflation, exp_value, sel_speed, chord, kin_viscosity, plot_type, format)
     
     plot_variable = 'CL_CD';
     plot_variable_printed_name = 'C_{L} / C_{D}';
 
+    if wingtype == "hard"
+
+        for j = 1:length(sel_speed) % looping over flow speed to create fixed-speed plots
+        
+            Re = sel_speed(j) * chord / kin_viscosity; % Reynolds number
+            disp(sel_speed(j))
+            
+            figure('Position', [200, 200, 1000, 1000])
+            
+            if plot_type == "title"
+                title([plot_variable_printed_name, ' plot # ', num2str(j), '; Flow Speed: ', num2str(sel_speed(j))],'fontweight','bold','fontsize', 24)
+            end
+    
+            hold on
+            grid on
+            xlabel('AoA [ ˚ ]','fontweight','bold','fontsize', 30);
+            ylabel(strcat(plot_variable_printed_name, ' [ ]'),'fontweight','bold','fontsize', 30);
+            ax = gca;
+            ax.XAxis.LineWidth = 2;
+            ax.YAxis.LineWidth = 2;
+            xlim([-10 30])
+            %ylim([-1.3 2])
+        
+            clear k1
+    
+    %         for k = 1:length(exp_value.f_avg)
+    %     
+    %              if (exp_value.vel(k) == sel_speed(j))
+    %                 if exist('k1','var') == 0
+    %                     errorbar(exp_value.aoa(k), (-exp_value.f_avg(k, lift_dir) / div(1, j)), exp_value.f_std(k, lift_dir) / div(1, j), 'ob', 'DisplayName', 'neutral', 'CapSize', 18, 'MarkerFaceColor', 'b', 'LineWidth', 1, MarkerEdgeColor = 'blue')
+    %                 else
+    %                     errorbar(exp_value.aoa(k), (-exp_value.f_avg(k, lift_dir) / div(1, j)), exp_value.f_std(k, lift_dir) / div(1, j), 'ob', 'HandleVisibility','off', 'CapSize', 18, 'MarkerFaceColor', 'b', 'LineWidth', 1, MarkerEdgeColor = 'blue')
+    %                     x_vec = [exp_value.aoa(k1), exp_value.aoa(k)];
+    %                     y_vec = [(-exp_value.f_avg(k1, lift_dir) / div(1, j)), (-exp_value.f_avg(k, lift_dir) / div(1, j))];
+    %                     plot(x_vec, y_vec, '--b', 'HandleVisibility','off')
+    %                 end
+    %                 k1 = k;
+    %              end
+    %         end
+    
+            for k = 1:length(exp_value.f_avg)
+        
+                 if (exp_value.vel(k) == sel_speed(j))
+                    if exist('k1','var') == 0
+                        errorbar(exp_value.aoa(k), -exp_value.f_ratio(k), exp_value.f_std_ratio(k), 'ob', 'DisplayName', 'neutral', 'CapSize', 18, 'MarkerFaceColor', 'b', 'LineWidth', 1, MarkerEdgeColor = 'blue')
+                    else
+                        errorbar(exp_value.aoa(k), -exp_value.f_ratio(k), exp_value.f_std_ratio(k), 'ob', 'HandleVisibility','off', 'CapSize', 18, 'MarkerFaceColor', 'b', 'LineWidth', 1, MarkerEdgeColor = 'blue')
+                        x_vec = [exp_value.aoa(k1), exp_value.aoa(k)];
+                        y_vec = [-exp_value.f_ratio(k1), -exp_value.f_ratio(k)];
+                        plot(x_vec, y_vec, '--b', 'HandleVisibility','off')
+                    end
+                    k1 = k;
+                 end
+            end
+    
+            str_annotation = sprintf('Re = %.2e', Re);
+            annotation('textbox', [0.696 0.77 0.1 0.1], 'String', str_annotation, 'BackgroundColor','white','LineStyle','-','Fontsize', 16, 'Interpreter','latex' ) % printing Re on plots
+            hold off
+            save_plot(wingtype, gcf, plot_type, plot_variable, j, sel_speed(j), format)
+        end
+
+  elseif wingtype == "soft"
+
     for j = 1:length(sel_speed) % looping over flow speed to create fixed-speed plots
-    
-        Re = sel_speed(j) * chord / kin_viscosity; % Reynolds number
-        disp(sel_speed(j))
-        
-        figure('Position', [200, 200, 1000, 1000])
-        
-        if plot_type == "title"
-            title([plot_variable_printed_name, ' plot # ', num2str(j), '; Flow Speed: ', num2str(sel_speed(j))],'fontweight','bold','fontsize', 24)
-        end
 
-        hold on
-        grid on
-        xlabel('AoA [ ˚ ]','fontweight','bold','fontsize', 30);
-        ylabel(strcat(plot_variable_printed_name, ' [ ]'),'fontweight','bold','fontsize', 30);
-        ax = gca;
-        ax.XAxis.LineWidth = 2;
-        ax.YAxis.LineWidth = 2;
-        xlim([-10 30])
-        %ylim([-1.3 2])
-    
-        clear k1
+    Re = sel_speed(j) * chord / kin_viscosity; % Reynolds number
+    disp(sel_speed(j))
 
-%         for k = 1:length(exp_value.f_avg)
-%     
-%              if (exp_value.vel(k) == sel_speed(j))
-%                 if exist('k1','var') == 0
-%                     errorbar(exp_value.aoa(k), (-exp_value.f_avg(k, lift_dir) / div(1, j)), exp_value.f_std(k, lift_dir) / div(1, j), 'ob', 'DisplayName', 'neutral', 'CapSize', 18, 'MarkerFaceColor', 'b', 'LineWidth', 1, MarkerEdgeColor = 'blue')
-%                 else
-%                     errorbar(exp_value.aoa(k), (-exp_value.f_avg(k, lift_dir) / div(1, j)), exp_value.f_std(k, lift_dir) / div(1, j), 'ob', 'HandleVisibility','off', 'CapSize', 18, 'MarkerFaceColor', 'b', 'LineWidth', 1, MarkerEdgeColor = 'blue')
-%                     x_vec = [exp_value.aoa(k1), exp_value.aoa(k)];
-%                     y_vec = [(-exp_value.f_avg(k1, lift_dir) / div(1, j)), (-exp_value.f_avg(k, lift_dir) / div(1, j))];
-%                     plot(x_vec, y_vec, '--b', 'HandleVisibility','off')
-%                 end
-%                 k1 = k;
-%              end
-%         end
+    clear k1 k2 k3 k4 k5
 
-        for k = 1:length(exp_value.f_avg)
-    
-             if (exp_value.vel(k) == sel_speed(j))
-                if exist('k1','var') == 0
-                    errorbar(exp_value.aoa(k), (-exp_value.f_ratio(k) / div(1, j)), exp_value.f_std_ratio(k) / div(1, j), 'ob', 'DisplayName', 'neutral', 'CapSize', 18, 'MarkerFaceColor', 'b', 'LineWidth', 1, MarkerEdgeColor = 'blue')
-                else
-                    errorbar(exp_value.aoa(k), (-exp_value.f_ratio(k) / div(1, j)), exp_value.f_std_ratio(k) / div(1, j), 'ob', 'HandleVisibility','off', 'CapSize', 18, 'MarkerFaceColor', 'b', 'LineWidth', 1, MarkerEdgeColor = 'blue')
-                    x_vec = [exp_value.aoa(k1), exp_value.aoa(k)];
-                    y_vec = [(-exp_value.f_ratio(k1) / div(1, j)), (-exp_value.f_ratio(k) / div(1, j))];
-                    plot(x_vec, y_vec, '--b', 'HandleVisibility','off')
-                end
-                k1 = k;
+    figure('Position', [200, 200, 1000, 1000])
+
+    if plot_type == "title"
+        title(['CL / CD plot # ', num2str(j), '; Flow Speed: ', num2str(sel_speed(j))],'fontweight', 'bold', 'fontsize', 24)
+    end
+
+    legend('Location','north','Orientation','horizontal','fontsize', 20)
+    hold on
+    grid on
+    xlabel('AoA [ ˚ ]','fontweight','bold','fontsize', 30);
+    ylabel(strcat(plot_variable_printed_name, ' [ ]'),'fontweight','bold','fontsize', 30);
+    xlim([-10 35])
+
+    for k = 1:length(exp_value.f_avg)
+        if (exp_value.vel(k) == sel_speed(j)) && (exp_value.inflation(k) == sel_inflation(1))
+            if exist('k1','var') == 0
+                        errorbar(exp_value.aoa(k), -exp_value.f_ratio(k), exp_value.f_std_ratio(k), 'ob', 'DisplayName', 'neutral', 'CapSize', 18, 'MarkerFaceColor', 'b', 'LineWidth', 1, MarkerEdgeColor = 'red')
+                    else
+                        errorbar(exp_value.aoa(k), -exp_value.f_ratio(k), exp_value.f_std_ratio(k), 'ob', 'HandleVisibility','off', 'CapSize', 18, 'MarkerFaceColor', 'b', 'LineWidth', 1, MarkerEdgeColor = 'red')
+                        x_vec = [exp_value.aoa(k1), exp_value.aoa(k)];
+                        y_vec = [-exp_value.f_ratio(k1), -exp_value.f_ratio(k)];
+                        plot(x_vec, y_vec, '--b', 'HandleVisibility','off')
              end
-        end
+             k1 = k;
 
-        str_annotation = sprintf('Re = %.2e', Re);
-        annotation('textbox', [0.696 0.77 0.1 0.1], 'String', str_annotation, 'BackgroundColor','white','LineStyle','-','Fontsize', 16, 'Interpreter','latex' ) % printing Re on plots
-        hold off
-        save_plot(gcf, plot_type, plot_variable, j, sel_speed(j), format)
+         elseif (exp_value.vel(k) == sel_speed(j)) && (exp_value.inflation(k) == sel_inflation(2))
+             if exist('k2','var') == 0
+             errorbar(exp_value.aoa(k), exp_value.f_ratio(k), exp_value.f_std_ratio(k), 'ok', 'DisplayName', 'inf. = 60 mL', 'CapSize', 18, 'MarkerFaceColor', 'k', 'LineWidth', 1, MarkerEdgeColor = 'black')
+
+             else
+                errorbar(exp_value.aoa(k), exp_value.f_ratio(k), exp_value.f_std_ratio(k), 'ok', 'HandleVisibility','off', 'CapSize', 18, 'MarkerFaceColor', 'k', 'LineWidth', 1, MarkerEdgeColor = 'black')
+                x_vec = [exp_value.aoa(k2), exp_value.aoa(k)];
+                y_vec = [exp_value.f_ratio(k2), exp_value.f_ratio(k)];
+                plot(x_vec, y_vec, '--k', 'HandleVisibility','off')
+            end
+            k2 = k;
+
+         elseif (exp_value.vel(k) == sel_speed(j)) && (exp_value.inflation(k) == sel_inflation(3))
+             if exist('k3','var') == 0
+             errorbar(exp_value.aoa(k), exp_value.f_ratio(k), exp_value.f_std_ratio(k), 'om', 'DisplayName', 'inf. = 90 mL', 'CapSize', 18, 'MarkerFaceColor', 'm', 'LineWidth', 1, MarkerEdgeColor = 'magenta')
+             else
+                errorbar(exp_value.aoa(k), exp_value.f_ratio(k), exp_value.f_std_ratio(k), 'om', 'HandleVisibility','off', 'CapSize', 18, 'MarkerFaceColor', 'm', 'LineWidth', 1, MarkerEdgeColor = 'magenta')
+                x_vec = [exp_value.aoa(k3), exp_value.aoa(k)];
+                y_vec = [exp_value.f_ratio(k3), exp_value.f_ratio(k)];
+                plot(x_vec, y_vec, '--m', 'HandleVisibility','off')
+            end
+            k3 = k;
+
+         elseif (exp_value.vel(k) == sel_speed(j)) && (exp_value.inflation(k) == sel_inflation(4))
+             if exist('k4','var') == 0
+             errorbar(exp_value.aoa(k), exp_value.f_ratio(k), exp_value.f_std_ratio(k), 'ob', 'DisplayName', 'inf. = 120 mL', 'CapSize', 18, 'MarkerFaceColor', 'b', 'LineWidth', 1, MarkerEdgeColor = 'blue')
+             else
+                errorbar(exp_value.aoa(k), exp_value.f_ratio(k), exp_value.f_std_ratio(k), 'ob', 'HandleVisibility','off', 'CapSize', 18, 'MarkerFaceColor', 'b', 'LineWidth', 1, MarkerEdgeColor = 'blue')
+                x_vec = [exp_value.aoa(k4), exp_value.aoa(k)];
+                y_vec = [exp_value.f_ratio(k4), exp_value.f_ratio(k)];
+                plot(x_vec, y_vec, '--b', 'HandleVisibility','off')
+            end
+            k4 = k;
+
+         elseif (exp_value.vel(k) == sel_speed(j)) && (exp_value.inflation(k) == sel_inflation(5))
+             if exist('k5','var') == 0
+            errorbar(exp_value.aoa(k), exp_value.f_ratio(k), exp_value.f_std_ratio(k), 'og', 'DisplayName', 'inf. = 30 mL', 'CapSize', 18, 'MarkerFaceColor', 'g', 'LineWidth', 1, MarkerEdgeColor = 'green')
+            else
+                errorbar(exp_value.aoa(k), exp_value.f_ratio(k), exp_value.f_std_ratio(k), 'og', 'HandleVisibility','off', 'CapSize', 18, 'MarkerFaceColor', 'b', 'LineWidth', 1, MarkerEdgeColor = 'green')
+                x_vec = [exp_value.aoa(k5), exp_value.aoa(k)];
+                y_vec = [exp_value.f_ratio(k5), exp_value.f_ratio(k)];
+                plot(x_vec, y_vec, '--g', 'HandleVisibility','off')
+            end
+            k5 = k;
+
+         end
+    end
+    
+    %str_annotation = sprintf('Re = %.2e', Re);
+    %annotation('textbox', [0.696 0.77 0.1 0.1], 'String', str_annotation, 'BackgroundColor','white','LineStyle','-','Fontsize', 16, 'Interpreter','latex' ) % printing Re on plots
+    hold off
+    save_plot(gcf, wingtype, plot_type, plot_variable, j, sel_speed(j), format)
+    %saveas(gcf, ['../pic/CL_over_CD_plot/', 'CL_over_CD_plot_#', num2str(j), 'flow_speed_0_', num2str(100 * sel_speed(j))], 'png'); % saving plots in desired folder
+    end
+
     end
 
 end
