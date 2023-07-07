17b45fb4c6a7b926f8beee26976f5faa05299e57
Andrea <a.giordano21@imperial.ac.uk>
2023-07-07 16:52:32 +0200

    code edit to allow softhard data saving

--- save_plot.m
+++ save_plot.m
@@ -22,8 +22,18 @@ function [] = save_plot(gcf, wingtype, plot_type, plot_variable, j, speed, forma
             saveas(gcf, ['./pictures_wing/pic_softwing/', date, '_', plot_type, '/', plot_variable, '_plot/', plot_variable, '_plot_#', num2str(j), '_flow_speed_0_', num2str(100 * speed)], format);
         
         end
-    end
 
+    elseif wingtype == "soft_hard"
+        if length(plot_type) == 5 && plot_type == "title"
+            saveas(gcf, ['./pictures_wing/pic_softhardwing/', date, '/', plot_variable, '_plot/', plot_variable, '_plot_#', num2str(j), '_flow_speed_0_', num2str(100 * speed)], format);
+        
+        elseif length(plot_type) == 7 && plot_type == "fourier"
+            saveas(gcf, ['./pictures_wing/pic_softhardwing/', date, '_', plot_type, '/', plot_variable, '_plot/', plot_variable, '_plot_', num2str(j), '_deg_flow_speed_0_', num2str(speed)], format);
+        else
+            saveas(gcf, ['./pictures_wing/pic_softhardwing/', date, '_', plot_type, '/', plot_variable, '_plot/', plot_variable, '_plot_#', num2str(j), '_flow_speed_0_', num2str(100 * speed)], format);
+        
+        end
+    end
 
 end
 
