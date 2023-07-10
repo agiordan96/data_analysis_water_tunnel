62b7efb31991452580cadfaf7e231d9a569b143a
Andrea <a.giordano21@imperial.ac.uk>
2023-07-10 18:01:27 +0200

    code correction for prompts

--- plot_soft_hard_wing.m
+++ plot_soft_hard_wing.m
@@ -13,14 +13,16 @@ lat_dir = sensor_orientation.lat_dir;
 CL_CD_prompt = "CL / CD plot: yes (1) or no (0)\n";
 CL_prompt = "CL plot: yes (1) or no (0)\n";
 CD_prompt = "CD plot: yes (1) or no (0)\n";
+avCL_avCD_prompt = "CD plot: yes (1) or no (0)\n";
+medianCL_CD_prompt = "CD plot: yes (1) or no (0)\n";
 
 plot_type_prompt = "please type in <title> or <no_title>\n";
 
 CL_CD_plot = (input(CL_CD_prompt, "s"));
 CL_plot = (input(CL_prompt, "s"));
 CD_plot = (input(CD_prompt, "s"));
-avCL_avCD_plot = (input(CD_prompt, "s"));
-medianCL_CD_plot = (input(CD_prompt, "s"));
+avCL_avCD_plot = (input(avCL_avCD_prompt, "s"));
+medianCL_CD_plot = (input(medianCL_CD_prompt, "s"));
 
 plot_type = (input(plot_type_prompt, "s"));
 
@@ -30,7 +32,7 @@ if CL_CD_plot == ""
 end
 
 if CL_plot == "" 
-   CL_plot = 1;    % enter default value here
+   CL_plot = 0;    % enter default value here
    fprintf('-> going for default value\n\n'); 
 end
 
@@ -40,12 +42,12 @@ if CD_plot == ""
 end
 
 if avCL_avCD_plot == "" 
-   avCL_avCD_plot = 0;     % enter default value here
+   avCL_avCD_plot = 1;     % enter default value here
    fprintf('-> going for default value\n\n'); 
 end
 
 if medianCL_CD_plot == "" 
-   medianCL_CD_plot = 0;     % enter default value here
+   medianCL_CD_plot = 1;     % enter default value here
    fprintf('-> going for default value\n\n'); 
 end
 
@@ -62,18 +64,25 @@ create_fold_save_pic(wingtype, plot_choice_vec, plot_type);
 %% Plotting
 
 if CL_CD_plot == 1
-    %folder_checking(plot_type, 'CL_CD');
     plot_CLCD(wingtype, sel_inflation, exp_value_soft, exp_value_hard, sel_speed, div, chord, kin_viscosity, plot_type, 'png', lift_dir, drag_dir);
 end
 
 if CL_plot == 1
-    %folder_checking(plot_type, 'CL');
     plot_CL(wingtype, sel_inflation, exp_value_soft, exp_value_hard, sel_speed, div, chord, kin_viscosity, lift_dir, plot_type, 'png');
 end
 
 if CD_plot == 1
-    %folder_checking(plot_type, 'CD');
     plot_CD(wingtype, sel_inflation, exp_value_soft, exp_value_hard, sel_speed, div, chord, kin_viscosity, drag_dir, plot_type, 'png');
 end
+
+if avCL_avCD_plot == 1
+    plot_avCL_avCD(wingtype, sel_inflation, exp_value_soft, exp_value_hard, sel_speed, div, chord, kin_viscosity, drag_dir, plot_type, 'png');
+end
+
+if medianCL_CD_plot == 1
+    plot_medianCLCD(wingtype, sel_inflation, exp_value_soft, exp_value_hard, sel_speed, div, chord, kin_viscosity, drag_dir, plot_type, 'png');
+end
+
+
 
 end
