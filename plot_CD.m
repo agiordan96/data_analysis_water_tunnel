597a73355fea35fb6dce01346b5445207a54330c
Andrea <a.giordano21@imperial.ac.uk>
2023-07-10 17:50:53 +0200

    code cleaning and Reynolds print correction

--- plot_CD.m
+++ plot_CD.m
@@ -8,7 +8,7 @@ function [] = plot_CD(wingtype, sel_inflation, exp_value, exp_value_hard, sel_sp
         for j = 1:length(sel_speed) % looping over flow speed to create fixed-speed plots
             
             Re = sel_speed(j) * chord / kin_viscosity; % Reynolds number
-            fprintf('plotting for Re = %.2d (vel = %.2d m/s)/n', Re, sel_speed(j))
+            fprintf('plotting for Re = %.2d (vel = %.2d m/s) \n', Re, sel_speed(j))
                  
             figure('Position', [200, 200, 1000, 1000])
             
@@ -57,7 +57,7 @@ function [] = plot_CD(wingtype, sel_inflation, exp_value, exp_value_hard, sel_sp
         for j = 1:length(sel_speed) % looping over flow speed to create fixed-speed plots
         
             Re = sel_speed(j) * chord / kin_viscosity; % Reynolds number
-            fprintf('plotting for Re = %.2d (vel = %.2d m/s)/n', Re, sel_speed(j))
+            fprintf('plotting for Re = %.2d (vel = %.2d m/s) \n', Re, sel_speed(j))
 
             clear k1 k2 k3 k4 k5
         
@@ -147,7 +147,7 @@ function [] = plot_CD(wingtype, sel_inflation, exp_value, exp_value_hard, sel_sp
         for j = 1:length(sel_speed) % looping over flow speed to create fixed-speed plots
         
             Re = sel_speed(j) * chord / kin_viscosity; % Reynolds number
-            fprintf('plotting for Re = %.2d (vel = %.2d m/s)/n', Re, sel_speed(j))
+            fprintf('plotting for Re = %.2d (vel = %.2d m/s) \n', Re, sel_speed(j))
             
             clear k1 k2 k3 k4 k5 hard1
         
