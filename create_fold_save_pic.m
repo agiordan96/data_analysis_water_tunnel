a67c65c38e8d330eaf0ad780fea860a8ce47d232
Andrea <a.giordano21@imperial.ac.uk>
2023-07-10 17:43:14 +0200

    edit to allow new folder saving

--- create_fold_save_pic.m
+++ create_fold_save_pic.m
@@ -6,9 +6,9 @@ function [] = create_fold_save_pic(wingtype, folder_vec, plot_type)
 
         if folder_vec(1) == 1 
             if plot_type == "no_title"
-                [~, ~, ~] = mkdir(strcat('./pictures_wing/pic_hardwing/', date, '_', plot_type, '/', 'CL_CD_plot/'));
+                [~, ~, ~] = mkdir(strcat('./pictures_wing/pic_hardwing/', date, '_', plot_type, '/', 'av_CL_CD_plot/'));
             else
-                [~, ~, ~] = mkdir(strcat('./pictures_wing/pic_hardwing/', date, '/CL_CD_plot/'));
+                [~, ~, ~] = mkdir(strcat('./pictures_wing/pic_hardwing/', date, '/av_CL_CD_plot/'));
             end
         end
         
@@ -28,13 +28,29 @@ function [] = create_fold_save_pic(wingtype, folder_vec, plot_type)
             end
         end
 
+        if folder_vec(4) == 1 
+            if plot_type == "no_title"
+                [~, ~, ~] = mkdir(strcat('./pictures_wing/pic_hardwing/', date, '_', plot_type, '/', 'avCL_avCD/'));
+            else
+                [~, ~, ~] = mkdir(strcat('./pictures_wing/pic_hardwing/', date, '/avCL_avCD/'));
+            end
+        end
+
+        if folder_vec(5) == 1 
+            if plot_type == "no_title"
+                [~, ~, ~] = mkdir(strcat('./pictures_wing/pic_hardwing/', date, '_', plot_type, '/', 'medianCL_CD_plot/'));
+            else
+                [~, ~, ~] = mkdir(strcat('./pictures_wing/pic_hardwing/', date, '/medianCL_CD_plot/'));
+            end
+        end
+
      elseif wingtype == "soft"
 
          if folder_vec(1) == 1 
             if plot_type == "no_title"
-                [~, ~, ~] = mkdir(strcat('./pictures_wing/pic_softwing/', date, '_', plot_type, '/', 'CL_CD_plot/'));
+                [~, ~, ~] = mkdir(strcat('./pictures_wing/pic_softwing/', date, '_', plot_type, '/', 'av_CL_CD_plot/'));
             else
-                [~, ~, ~] = mkdir(strcat('./pictures_wing/pic_softwing/', date, '/CL_CD_plot/'));
+                [~, ~, ~] = mkdir(strcat('./pictures_wing/pic_softwing/', date, '/av_CL_CD_plot/'));
             end
         end
         
@@ -53,13 +69,29 @@ function [] = create_fold_save_pic(wingtype, folder_vec, plot_type)
                 [~, ~, ~] = mkdir(strcat('./pictures_wing/pic_softwing/', date, '/CD_plot/'));
             end
         end
+
+        if folder_vec(4) == 1 
+            if plot_type == "no_title"
+                [~, ~, ~] = mkdir(strcat('./pictures_wing/pic_softwing/', date, '_', plot_type, '/', 'avCL_avCD_plot/'));
+            else
+                [~, ~, ~] = mkdir(strcat('./pictures_wing/pic_softwing/', date, '/avCL_avCD_plot/'));
+            end
+        end
+
+        if folder_vec(5) == 1 
+            if plot_type == "no_title"
+                [~, ~, ~] = mkdir(strcat('./pictures_wing/pic_softwing/', date, '_', plot_type, '/', 'medianCL_CD_plot/'));
+            else
+                [~, ~, ~] = mkdir(strcat('./pictures_wing/pic_softwing/', date, '/medianCL_CD_plot/'));
+            end
+        end
     
     elseif wingtype == "soft_hard"
         if folder_vec(1) == 1 
             if plot_type == "no_title"
-                [~, ~, ~] = mkdir(strcat('./pictures_wing/pic_softhardwing/', date, '_', plot_type, '/', 'CL_CD_plot/'));
+                [~, ~, ~] = mkdir(strcat('./pictures_wing/pic_softhardwing/', date, '_', plot_type, '/', 'av_CL_CD_plot/'));
             else
-                [~, ~, ~] = mkdir(strcat('./pictures_wing/pic_softhardwing/', date, '/CL_CD_plot/'));
+                [~, ~, ~] = mkdir(strcat('./pictures_wing/pic_softhardwing/', date, '/av_CL_CD_plot/'));
             end
         end
         
@@ -76,6 +108,22 @@ function [] = create_fold_save_pic(wingtype, folder_vec, plot_type)
                 [~, ~, ~] = mkdir(strcat('./pictures_wing/pic_softhardwing/', date, '_', plot_type, '/', 'CD_plot/'));
             else
                 [~, ~, ~] = mkdir(strcat('./pictures_wing/pic_softhardwing/', date, '/CD_plot/'));
+            end
+        end
+
+        if folder_vec(4) == 1 
+            if plot_type == "no_title"
+                [~, ~, ~] = mkdir(strcat('./pictures_wing/pic_softhardwing/', date, '_', plot_type, '/', 'avCL_avCD_plot/'));
+            else
+                [~, ~, ~] = mkdir(strcat('./pictures_wing/pic_softhardwing/', date, '/avCL_avCD_plot/'));
+            end
+        end
+
+        if folder_vec(5) == 1 
+            if plot_type == "no_title"
+                [~, ~, ~] = mkdir(strcat('./pictures_wing/pic_softhardwing/', date, '_', plot_type, '/', 'medianCL_CD_plot/'));
+            else
+                [~, ~, ~] = mkdir(strcat('./pictures_wing/pic_softhardwing/', date, '/medianCL_CD_plot/'));
             end
         end
     end
